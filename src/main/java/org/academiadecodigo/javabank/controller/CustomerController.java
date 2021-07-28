9669aa69f65809e6386f15e2032273fbcbc74184
igreja94 <daniel_contas@hotmail.com>
2021-07-28 14:34:09 +0100

    added view and delete to javabank

--- src/main/java/org/academiadecodigo/javabank/controller/CustomerController.java
+++ src/main/java/org/academiadecodigo/javabank/controller/CustomerController.java
@@ -5,8 +5,10 @@ import org.academiadecodigo.javabank.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Controller responsible for rendering {@link Customer} related views
@@ -38,4 +40,14 @@ public class CustomerController {
         model.addAttribute("customers", customerService.list());
         return "customer/list";
     }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public String getCustomer(@PathVariable Integer id, Model model) {
+
+        model.addAttribute("customer",customerService.get(id));
+
+        return "customer/viewcustomer";
+
+    }
+
 }
